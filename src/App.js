3fd089ad9121b73b8079fdc0f2027191927f2f40
timurturbil <timurturbil@hotmail.com>
2021-03-11 23:28:27 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.scss';
+import { useState } from 'react';
+const App = () => {
+  const [temperature, setTemperature] = useState(0.0);
+  const [name, setName] = useState("madrid");
+  const [air, setAir] = useState("Clear")
+  fetch(`http://api.weatherapi.com/v1/forecast.json?key=b5d717f75dcc4256bb7191833211103&q=${name}&days=7`)
+    .then(response => response.json())
+    .then(data => {
+      setTemperature(data.current.temp_c);
+      setAir(data.current.condition.icon)
+    }).catch(function () {
+      console.log("error")
+    });
+  const handleChange = (input) => {
+    setName(input.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="weather-wrapper">
+        <div className="weather-card">
+          <img className="image" src={air} />,
+          <h1>{temperature}</h1>
+          <p>{name}</p>
+        </div>
+      </div>
+      <div className="inputText">
+        <input type="text" onChange={handleChange} value={name} />
+      </div>
     </div>
   );
 }
